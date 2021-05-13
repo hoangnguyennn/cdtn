89fe8aff278a83c00e152ba0ca8f9d1cda02a830
hoangnguyen <nvanhoang188@gmail.com>
2021-05-14 00:10:48 +0700

    create Mobile Menu component

--- packages/client/pages/index.tsx
+++ packages/client/pages/index.tsx
@@ -1,9 +1,11 @@
+import MobileMenu from '../components/Menu/Mobile';
 import ProductList from '../components/Product/List';
 
 const Home = () => {
 	return (
 		<div className="container">
 			<ProductList columns={1} lg-columns={4} title="Trending products" />
+			<MobileMenu />
 		</div>
 	);
 };
