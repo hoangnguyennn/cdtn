38fc7f79c357191ee6a1908567d4c5012ec8a7c4
hoangnguyen <nvanhoang188@gmail.com>
2021-06-09 19:28:54 +0700

    be: fix filter invalid fields

--- packages/server/src/utils/index.ts
+++ packages/server/src/utils/index.ts
@@ -1,6 +1,6 @@
 export const removeInvalidFields = function <T>(obj: T): T {
 	const result = Object.entries(obj).reduce((result: any, [key, value]) => {
-		if (value !== undefined && value !== null) {
+		if (value !== undefined && value !== null && value !== '') {
 			result[key] = value;
 		}
 
